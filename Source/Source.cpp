1884bef0101cd7382ad4ec67fba45562d9db416b
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2026-04-21 15:33:38 -0300

    Creating the food

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -1,21 +1,59 @@
 #include <raylib.h>
+#include <iostream>
 
-#define SCREEN_X 750   
-#define SCREEN_Y 750
 #define WINDOW_TITLE "Snake Game - VM Games"
 
 Color green = { 173, 204, 96, 255 };
 Color darkGreen = { 43, 51, 24, 255 };
 
+int cellSize = 30;
+int cellCount = 25;
+
+class Food
+{
+public:
+    Vector2 position;
+	Texture2D texture;
+    Food()
+    {
+        Image image = LoadImage("Graphics/food.png");
+		texture = LoadTextureFromImage(image);
+		UnloadImage(image);
+		position = GenerateRandomPos();
+	}
+
+    ~Food()
+    {
+		UnloadTexture(texture);
+    }
+
+    void Draw()
+    {
+        DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
+	}
+
+    Vector2 GenerateRandomPos()
+    {
+		float x = GetRandomValue(0, cellCount - 1);
+		float y = GetRandomValue(0, cellCount - 1);
+
+		return { x, y };
+    }
+};
+
 int main()
 {
-    InitWindow(SCREEN_X, SCREEN_Y, WINDOW_TITLE);
+    InitWindow(cellSize*cellCount, cellSize * cellCount, WINDOW_TITLE);
+	SetTargetFPS(60);
+
+	Food food = Food();
 
     // Main game loop
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(BLACK);
+		food.Draw();
+        ClearBackground(green);
         EndDrawing();    
     }
     CloseWindow();  
